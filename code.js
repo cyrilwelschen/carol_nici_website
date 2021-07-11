9fef69f4f3ffef789826977273cc05a908aabd0a
Cyril Welschen <cj.welschen@gmail.com>
2021-07-11 20:19:26 +0200

    korrekturen Rachel

--- code.js
+++
@@ -1,9 +0,0 @@
-closeSidebar();
-
-function openSidebar() {
-    document.getElementById("mySidebar").style.display = "block";
-}
-
-function closeSidebar() {
-    document.getElementById("mySidebar").style.display = "none";
-}
